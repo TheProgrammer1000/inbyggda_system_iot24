d3404ee02ab2f62b736a555691023fabe17e5d99
Dennis <112337016+TheProgrammer1000@users.noreply.github.com>
2025-02-15 22:57:48 +0100

    Setup the constructor to on analog log to get already constructionaized adc and binaryLed

--- uppgift/uppgift_1/components/analog_led/analog_led.cpp
+++ uppgift/uppgift_1/components/analog_led/analog_led.cpp
@@ -2,27 +2,8 @@
 
 namespace myAnalogLed {
 
-    void analogLed::config() {
-        analogLed::adcInstance = (adc_t) malloc(sizeof(adc_t));
-        analogLed::binaryLedInstance = (binaryLed_t) malloc(sizeof(binaryLed_t));
-
-        
-    }
-    
     void analogLed::init() {
-       // this->adcInstance.init();
-        //this->binaryLedInstance.init();
+ 
     }
 
-
-    // analogLed::analogLed() {
-    //     (this->instance)(MY_ADC_UNIT, MY_DIGI_CLK_SOURCE, ADC_ULP_MODE_DISABLE);
-    //     //this->myAdc(MY_ADC_UNIT, MY_DIGI_CLK_SOURCE, ADC_ULP_MODE_DISABLE);
-        
-
-    //     //this->myAdc.init();
-
-    //     // myBinaryLed::binaryLed binaryLed1(3, LEDC_CHANNEL_0, LEDC_TIMER_12_BIT, 0, LEDC_INTR_DISABLE);
-    //     // binaryLed1.init();
-    // }
 }
--- uppgift/uppgift_1/components/analog_led/include/analog_led.h
+++ uppgift/uppgift_1/components/analog_led/include/analog_led.h
@@ -7,26 +7,13 @@
 namespace myAnalogLed {
     class analogLed {
         private:
-            adcOneMode::adc* adcInstance;
-            myBinaryLed::binaryLed* binaryLedInstance;
+            adcOneMode::adc adcInstance;
+            myBinaryLed::binaryLed binaryLedInstance;
 
         public:
-            analogLed() : adcInstance(nullptr), binaryLedInstance(nullptr) {}//adcInstance(MY_ADC_UNIT, MY_DIGI_CLK_SOURCE, ADC_ULP_MODE_DISABLE), binaryLedInstance(3, LEDC_CHANNEL_0, LEDC_TIMER_12_BIT, 0, LEDC_INTR_DISABLE) {
+            analogLed(adcOneMode::adc& adcInstance, myBinaryLed::binaryLed& binaryLedInstance) : adcInstance(adcInstance), binaryLedInstance(binaryLedInstance) {}//adcInstance(MY_ADC_UNIT, MY_DIGI_CLK_SOURCE, ADC_ULP_MODE_DISABLE), binaryLedInstance(3, LEDC_CHANNEL_0, LEDC_TIMER_12_BIT, 0, LEDC_INTR_DISABLE) {
 
-            typedef adcOneMode::adc* adc_t;
-            typedef myBinaryLed::binaryLed* binaryLed_t;
 
-            // analogLed::~analogLed() {
-            //     if (adcInstance) {
-            //         delete adcInstance;
-            //         adcInstance = nullptr;
-            //     }
-            //     if(binaryLedInstance) {
-            //         delete binaryLedInstance;
-            //         binaryLedInstance = nullptr;
-            //     }
-            // }
-            void config();
             void init();
     };
 }
