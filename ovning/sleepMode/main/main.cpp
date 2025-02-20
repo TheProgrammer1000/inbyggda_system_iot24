dcda576ceb420ac5364385334d31aad30a992279
Dennis <112337016+TheProgrammer1000@users.noreply.github.com>
2025-02-20 12:56:21 +0100

    Finished did bonus on sleepMode!

--- ovning/sleepMode/main/main.cpp
+++ ovning/sleepMode/main/main.cpp
@@ -5,12 +5,18 @@
 #include "freertos/timers.h"
 #include "binary_led.h"
 #include "printer.h"
+#include "button.h"
 
+#define RTC_PIN GPIO_NUM_3
 /*
 Bonus:
 Gå I sleep mode och vakna med knapptryck samtidigt som du har en lampa som ska gå från av till på eller på till av en gang I sekunden
 Du får inte missa att blinka oavsett hur mycket du trycker på knappen
 */
+
+void buttonFunc(int pin) {
+    printf("Button is pressed!: %d\n", pin);
+}
 
 extern "C" void app_main(void)
 {
@@ -18,12 +24,31 @@ extern "C" void app_main(void)
     binaryLed1.init();
     binaryLed1.blink(1000, 1000);
 
+    my_button::button button1(3, GPIO_PULLUP_DISABLE, GPIO_PULLDOWN_ENABLE, GPIO_INTR_DISABLE);
+    button1.init();
+
+    button1.setOnPressed(buttonFunc);
+   
+  
+
     //unsigned int secound = 1000000; // one secound
    
 
     //int count = 0;
 
    
+    if(esp_sleep_is_valid_wakeup_gpio((gpio_num_t) button1.get_pin())) {
+        ESP_ERROR_CHECK(esp_sleep_enable_ext1_wakeup((1UL << button1.get_pin()), ESP_EXT1_WAKEUP_ANY_HIGH));
+
+        ESP_ERROR_CHECK(esp_light_sleep_start());
+
+        PRINTF_COLOR(ANSI_BLUE, "Success!" NEW_LINE);
+    }
+
+
+
+
+    //esp_sleep_enable_ext1_wakeup();
 
     //esp_sleep_wakeup_cause_t wakeType = esp_sleep_get_wakeup_cause();
 
@@ -35,7 +60,8 @@ extern "C" void app_main(void)
         //count++;
 
         binaryLed1.update();
-        //ESP_ERROR_CHECK(esp_light_sleep_start());
+        button1.update();
+      
 
         //if(count == 10) {
             //esp_deep_sleep(3000000);
