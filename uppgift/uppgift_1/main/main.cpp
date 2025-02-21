0f65bedd607c1c4efc8b6d53a22cc88df73b7efe
Dennis <112337016+TheProgrammer1000@users.noreply.github.com>
2025-02-21 15:08:06 +0100

    Done with nvs component

--- uppgift/uppgift_1/main/main.cpp
+++ uppgift/uppgift_1/main/main.cpp
@@ -1,7 +1,5 @@
-#pragma once
-#include <stdio.h>
-#include "binary_led.h"
-#include "analog_led.h"
+#include "nvs.h"
+#include "esp_log.h"
 
 
 void myFunctionToAnalog(char* msg) {
@@ -16,34 +14,17 @@ void myFunctionToPotenti() {
 
 extern "C" void app_main(void)
 {  
-
-    //myAnalogLed::analogLed analogLed1(3, LEDC_CHANNEL_0, LEDC_TIMER_10_BIT, 0b1111111111, LEDC_INTR_DISABLE);
-    //analogLed1.init();
-    //analogLed1.setLed(myFunctionToAnalog);
+    myNvs::nvs nvs;
     
+    nvs.init();
+    //ESP_ERROR_CHECK(nvs_erase_all(nvs.getNvsHandle()));
+    //nvs.init();
+    // printf("my serialValue: %s\n",nvs.getSerialNumber());
+    // printf("my deviceNAme: %s\n",nvs.getDeviceName());
 
-    //
-    //analogLed1.setLed(50);
-    //analogLed1.sineWave(pdMS_TO_TICKS(2000));
-
-    //adcOneMode::adc adc1(ADC_UNIT_1, ADC_DIGI_CLK_SRC_DEFAULT, ADC_ULP_MODE_DISABLE);
-
-    //adc1.init();
-    //adc1.setOnThreshold(2000, true, myFunctionToPotenti);
+    //nvs.setDeviceName("myDev2");
+    //nvs.setSerialNumber("AA:BB:CC:DD22");
 
 
-    //myBinaryLed::binaryLed binaryLed1(3, GPIO_PULLUP_DISABLE, GPIO_PULLDOWN_DISABLE, GPIO_INTR_DISABLE);
-
-    //binaryLed1.init();
-    //binaryLed1.blink(2000, 2000);
-    //binaryLed1.setLed(1);    
-    
-
-    
-    while(1) {
-        //binaryLed1.update();
-        //adc1.update();
-        //analogLed1.update();
-        vTaskDelay(pdMS_TO_TICKS(30));
-    }
+    //printf("\nHello from main!\n");
 }
