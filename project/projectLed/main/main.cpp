82fa4dd4008175b44cac1ff5b7f20bc3cb58bd3a
Dennis <112337016+TheProgrammer1000@users.noreply.github.com>
2025-03-12 13:32:24 +0100

    Made a task for movment_sensor decection

--- project/projectLed/main/main.cpp
+++ project/projectLed/main/main.cpp
@@ -11,10 +11,33 @@
 #include "esp_sleep.h"
 
 
+void task_movement_sensor_detection(void* pvParameter) {
+    
+    myGpio::Gpio* gpio6 = (myGpio::Gpio*) pvParameter;
+
+    PRINTF_COLOR(ANSI_BLUE, "gpio pin: %d" NEW_LINE, gpio6->getPin());
+
+
+    while (1)
+    {
+        int sensorMovementLevel = gpio_get_level((gpio_num_t) gpio6->getPin());
+
+        if(sensorMovementLevel == 0) {
+            PRINTF_COLOR(ANSI_MAGENTA, "Movement not detected: %d" NEW_LINE, sensorMovementLevel);
+        }
+        else {
+            PRINTF_COLOR(ANSI_MAGENTA, "Movement detected!!: %d" NEW_LINE, sensorMovementLevel);
+       }
+        //PRINTF_COLOR(ANSI_BLUE,"HELLO!" NEW_LINE);
+        vTaskDelay(pdMS_TO_TICKS(30));
+    }
+    
+}
 
 void onPressedButton(int pin) {
     PRINTF_COLOR(ANSI_BLUE, "BUTTON PRESSED PIN: %d" NEW_LINE, pin);
 }
+
 
 extern "C" void app_main(void)
 {
@@ -32,43 +55,46 @@ extern "C" void app_main(void)
     myAnalogLed::analogLed analogLed1(5, ledcTimerConf.timer_num, LEDC_CHANNEL_2, LEDC_TIMER_12_BIT, 0b111111111111, LEDC_INTR_DISABLE);    
     adcOneMode::adc adc1(ADC_UNIT_1, ADC_DIGI_CLK_SRC_DEFAULT, ADC_ULP_MODE_DISABLE, ADC_CHANNEL_2, ADC_BITWIDTH_12, ADC_ATTEN_DB_12);
     my_button::button button1(4, GPIO_PULLUP_DISABLE, GPIO_PULLDOWN_ENABLE, GPIO_INTR_DISABLE);
-    
+    myGpio::Gpio gpio6(6, GPIO_MODE_INPUT, GPIO_PULLUP_DISABLE, GPIO_PULLDOWN_ENABLE, GPIO_INTR_DISABLE);
     
     analogLed1.init();
     adc1.init();
     button1.init();
+    gpio6.init();
     
-    
-
     button1.onPressed_cb = onPressedButton;
-
-
-
     ESP_ERROR_CHECK(esp_deep_sleep_enable_gpio_wakeup((1UL << button1.get_pin()), ESP_GPIO_WAKEUP_GPIO_HIGH));
-
 
     static TickType_t startTick = xTaskGetTickCount();
 
+
+    xTaskCreate(task_movement_sensor_detection, "MovmentSensor", 4096, &gpio6, 2, NULL);
+
     while (1)
     {   
+
+   
+        
+
+
         TickType_t elaspedTickSinceStart = xTaskGetTickCount();
 
         
-        if(elaspedTickSinceStart - startTick >= pdMS_TO_TICKS(20000)) {
+        // if(elaspedTickSinceStart - startTick >= pdMS_TO_TICKS(20000)) {
             
-            ESP_ERROR_CHECK(esp_deep_sleep_try_to_start());
+        //     ESP_ERROR_CHECK(esp_deep_sleep_try_to_start());
         
-            startTick = xTaskGetTickCount();
-        }
+        //     startTick = xTaskGetTickCount();
+        // }
 
-        button1.update();
+        // button1.update();
 
-        int LDRInputResistans = adc1.getVoltageValueFromLDR();
+        // int LDRInputResistans = adc1.getVoltageValueFromLDR();
 
-        if(LDRInputResistans != -1) {
-            analogLed1.setLed(LDRInputResistans);
-            analogLed1.update();
-        }
+        // if(LDRInputResistans != -1) {
+        //     analogLed1.setLed(LDRInputResistans);
+        //     analogLed1.update();
+        // }
 
         vTaskDelay(pdMS_TO_TICKS(30));
     }
